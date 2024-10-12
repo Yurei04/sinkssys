30b64d4274f0b1480deca804d33869671e36c905
Yuri <142381932+Yurei04@users.noreply.github.com>
2024-10-12 19:13:00 +0800

    modal

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function goHome() {
     window.location.href = "./home.html";
 }
+
+function openModal(id) {
+    document.getElementById(id).classList.remove("hidden");
+    document.getElementById(id).classList.add("flex");
+}
